fcda84e08ae03f3066b4e390dabb96b1ad70de8a
waltertaya <waltermittybecker5@gmail.com>
2025-03-09 10:59:37 +0300

    Learning datatypes

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Go Tutorial
 
+### Common datatypes
+- int
+- uint
+- float64
+- complex128
+- bool
+- string
+
+- **const**
+
 ## Author
 
 - [waltertaya](https://github.com/waltertaya)
--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, there friend!")
+	// var hello string = "Hello there friend"
+	hello := "Hello there friend" // Infered type
+	fmt.Println(hello)
+
+	num1 := 2.3
+	fmt.Printf("%T", num1)
+
+	const pi = 22/7 // const be known during compile
 }
