50b22ba1e278dfd2236725308d738b9555d3c0e2
waltertaya <waltermittybecker5@gmail.com>
2025-03-10 13:52:32 +0300

    Add constants for PI, Language, and MaxThread in main.go

---
+++ Go Constants/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	PI float64 = 3.14159
+	Language = "Go"
+	MaxThread = 10
+)
+
+func main() {
+	fmt.Println(PI)
+	fmt.Println(Language)
+	fmt.Println(MaxThread)
+}
